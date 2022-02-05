b0c0c56ec8c0d22622496434acec9017eae494df
niktar96 <shungo724@gmail.com>
2022-02-05 11:23:22 +0900

    Create amplify studio test!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import { CardBCollection, NavBar, MarketingFooter } from './ui-components';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
+      <CardBCollection />
+      <MarketingFooter />
     </div>
   );
 }
